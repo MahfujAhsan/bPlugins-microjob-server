0f7918b3407257789733b16c23a0b418d99e3f36
MahfujAhsan <ahsanmahfuj@gmail.com>
2023-01-18 06:50:26 +0600

    user updated

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 require('dotenv').config();
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 var jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
@@ -11,10 +10,38 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const uri = `mongodb+srv://${process.env.DB_ADMIN}:${process.env.DB_SECURITY_KEY}@bpluginsmicrojob.lwwmdtk.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
 async function run() {
-    try{
-        await client.connect()
+    try {
+        await client.connect();
+        const usersCollection = client.db('bPluginsMicrojobDatabase').collection('usersCollection');
+
+        app.put('/user/:email', async (req, res) => {
+            const email = req.params.email;
+            const user = req.body;
+            const filter = { email: email };
+            const options = { upsert: true };
+            const updatedDoc = {
+                $set: user
+            };
+            const result = await usersCollection.updateOne(filter, updatedDoc, options);
+            const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_KEY, { expiresIn: '24h' });
+            res.send({ result, token });
+        });
     } finally {
 
     };
-}
+};
+
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+    res.send('bPlugins MicroJob Server Up')
+});
+
+app.listen(port, () => {
+    console.log('bPlugins MicroJob Server Pumping')
+})
+

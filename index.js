714e10c8b7794cfdf9f9a36d25274373dd111790
kamran-hussen <kamran.techydevs@gmail.com>
2023-03-05 23:37:41 +0600

    jobboy day

--- index.js
+++ index.js
@@ -10,13 +10,13 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_ADMIN}:${process.env.DB_SECURITY_KEY}@bpluginsmicrojob.lwwmdtk.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_ADMIN}:${process.env.DB_SECURITY_KEY}@clusterjobboy.xkxgypm.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run() {
     try {
-        await client.connect();
         const usersCollection = client.db('bPluginsMicrojobDatabase').collection('usersCollection');
+        const usersInfo = client.db('bPluginsMicrojobDatabase').collection('usersInfo');
 
         app.put('/user/:email', async (req, res) => {
             const email = req.params.email;
@@ -27,6 +27,7 @@ async function run() {
                 $set: user
             };
             const result = await usersCollection.updateOne(filter, updatedDoc, options);
+
             const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_KEY, { expiresIn: '24h' });
             res.send({ result, token });
         });
